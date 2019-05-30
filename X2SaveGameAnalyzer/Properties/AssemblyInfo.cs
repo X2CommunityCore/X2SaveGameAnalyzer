4655e78930580682fae7bab5e60a1866f022bc37
RevZero <936992+RevZero@users.noreply.github.com>
2019-05-30 22:19:18 +0200

    Release preparations.

--- X2SaveGameAnalyzer/Properties/AssemblyInfo.cs
+++ X2SaveGameAnalyzer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("X2SaveGameAnalyzer")]
+[assembly: AssemblyTitle("XCOM 2 Savegame Analyzer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("X2SaveGameAnalyzer")]
+[assembly: AssemblyProduct("XCOM 2 Savegame Analyzer")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
